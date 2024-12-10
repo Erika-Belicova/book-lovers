7fc9ff490c44a16f813ef61f964bbac563d0f73d
Erika Belicova <erika.belicov@gmail.com>
2024-12-10 13:24:04 +0100

    config change production setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+  config.action_mailer.default_url_options = { host: "https://book-lovers-erika-01bff80f004a.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
